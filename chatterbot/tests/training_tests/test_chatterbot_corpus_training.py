0a3a96d6e9407775f6443daf1b9ab9c3c2837c3c
cptsaheb <40605571+cptsaheb@users.noreply.github.com>
2018-10-26 14:38:15 +0530

    MyChatBot

---
+++ chatterbot/tests/training_tests/test_chatterbot_corpus_training.py
@@ -0,0 +1,48 @@
+from tests.base_case import ChatBotTestCase
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
+class ChatterBotCorpusTrainingTestCase(ChatBotTestCase):
+    """
+    Test case for training with data from the ChatterBot Corpus.
+
+    Note: This class has a mirror tests_django/integration_tests/
+    """
+
+    def setUp(self):
+        super(ChatterBotCorpusTrainingTestCase, self).setUp()
+        self.trainer = ChatterBotCorpusTrainer(
+            self.chatbot,
+            show_training_progress=False
+        )
+
+    def test_train_with_english_greeting_corpus(self):
+        self.trainer.train('chatterbot.corpus.english.greetings')
+
+        results = self.chatbot.storage.filter(text='Hello')
+
+        self.assertGreater(len(results), 1)
+
+    def test_train_with_english_greeting_corpus_tags(self):
+        self.trainer.train('chatterbot.corpus.english.greetings')
+
+        results = self.chatbot.storage.filter(text='Hello')
+
+        self.assertGreater(len(results), 1)
+        statement = results[0]
+        self.assertEqual(['greetings'], statement.get_tags())
+
+    def test_train_with_multiple_corpora(self):
+        self.trainer.train(
+            'chatterbot.corpus.english.greetings',
+            'chatterbot.corpus.english.conversations',
+        )
+        results = self.chatbot.storage.filter(text='Hello')
+
+        self.assertGreater(len(results), 1)
+
+    def test_train_with_english_corpus(self):
+        self.trainer.train('chatterbot.corpus.english')
+        results = self.chatbot.storage.filter(text='Hello')
+
+        self.assertGreater(len(results), 1)
